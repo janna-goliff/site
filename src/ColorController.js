c7f9a83354dcc9003fe332f21a867fe1a62809c7
janna-goliff <gojanna@sas.upenn.edu>
2021-01-13 15:49:10 -0500

    implemented color changing

--- src/ColorController.js
+++ src/ColorController.js
@@ -1,93 +1,140 @@
 import React from 'react';
 import './App.css';
 import './ColorController.css';
-
-const colors = [
-  'black',
-  '#E73333',
-  '#FB881E',
-  '#E5BA21',
-  '#43704D',
-  '#25498E',
-  '#8E2571',
-];
+import { colors, colorsRGBA } from './colors.js';
 
 class ColorController extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isToggleOn: true };
-
-    // This binding is necessary to make `this` work in the callback
+    this.state = { selected: 4 };
     this.handleClick = this.handleClick.bind(this);
+    this.handleMouseEnter = this.handleMouseEnter.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
   }
 
   handleClick(e, num) {
     e.preventDefault();
+    this.setState({
+      selected: num,
+    });
+    this.props.sendData(this.state.selected);
     // console.log('was clicked.');
     // console.log(num);
+    // for (let i = 0; i < colors.length; i++) {
+    //   const currColorSelector = document.getElementById('colorOuter' + `${i}`);
+    //   if (i === num) {
+    //     // changes to correct color
+    //     currColorSelector.style.backgroundColor = colors[i];
+    //   } else {
+    //     // resets all others
+    //     currColorSelector.style.backgroundColor = 'white';
+    //   }
+    // }
+  }
+
+  handleMouseEnter(e, num) {
+    e.preventDefault();
+    const currColorSelector = document.getElementById('colorOuter' + `${num}`);
+    currColorSelector.style.boxShadow = `0 0 2px 2px ${colorsRGBA[num]}`;
+  }
+
+  handleMouseLeave(e, num) {
+    e.preventDefault();
+    const currColorSelector = document.getElementById('colorOuter' + `${num}`);
+    currColorSelector.style.boxShadow = `0 2px 4px 0 rgba(0, 0, 0, 0.15)`;
+  }
+
+  componentDidMount() {
+    const outerWrapper = document.getElementById('outerWrapper');
+    // outerWrapper.style.backgroundColor = colors[this.state.selected];
     for (let i = 0; i < colors.length; i++) {
       const currColorSelector = document.getElementById('colorOuter' + `${i}`);
-      if (i === num) {
-        // changes to correct color
-        currColorSelector.style.backgroundColor = colors[i];
-      } else {
-        // resets all others
-        currColorSelector.style.backgroundColor = 'white';
-      }
+      currColorSelector.style.backgroundColor = colors[i];
+
+      currColorSelector.addEventListener('click', (e) =>
+        this.handleClick(e, i)
+      );
+      currColorSelector.addEventListener('mouseenter', (e) =>
+        this.handleMouseEnter(e, i)
+      );
+      currColorSelector.addEventListener('mouseleave', (e) =>
+        this.handleMouseLeave(e, i)
+      );
     }
+  }
+
+  componentDidUpdate() {
+    console.log(`${this.state.selected}`);
   }
 
   render() {
     return (
-      <div id='colorFormWrapper'>
-        <div
-          className='colorFormOutline'
-          id='colorOuter0'
-          onClick={(e) => this.handleClick(e, 0)}
-        >
-          {/* <div className='colorFormInner' /> */}
-        </div>
-        <div
-          className='colorFormOutline'
-          id='colorOuter1'
-          onClick={(e) => this.handleClick(e, 1)}
-        >
-          {/* <div className='colorFormInner' /> */}
-        </div>
-        <div
-          className='colorFormOutline'
-          id='colorOuter2'
-          onClick={(e) => this.handleClick(e, 2)}
-        >
-          {/* <div className='colorFormInner' /> */}
-        </div>
-        <div
-          className='colorFormOutline'
-          id='colorOuter3'
-          onClick={(e) => this.handleClick(e, 3)}
-        >
-          {/* <div className='colorFormInner' /> */}
-        </div>
-        <div
-          className='colorFormOutline'
-          id='colorOuter4'
-          onClick={(e) => this.handleClick(e, 4)}
-        >
-          {/* <div className='colorFormInner' /> */}
-        </div>
-        <div
-          className='colorFormOutline'
-          id='colorOuter5'
-          onClick={(e) => this.handleClick(e, 5)}
-        >
-          {/* <div className='colorFormInner' /> */}
-        </div>
-        <div
-          className='colorFormOutline'
-          id='colorOuter6'
-          onClick={(e) => this.handleClick(e, 6)}
-        >
-          {/* <div className='colorFormInner' /> */}
+      <div id='outerWrapper'>
+        <p id='colorFormLabel'>choose a different color!</p>
+        <div id='colorFormWrapper'>
+          <div
+            className='colorFormOutline'
+            id='colorOuter0'
+            // onClick={(e) => this.handleClick(e, 0)}
+            // onMouseEnter={(e) => this.handleMouseEnter(e, 0)}
+            // onMouseLeave={(e) => this.handleMouseLeave(e, 0)}
+          >
+            {/* <div className='colorFormInner' /> */}
+          </div>
+          <div
+            className='colorFormOutline'
+            id='colorOuter1'
+            // onClick={(e) => this.handleClick(e, 1)}
+            // onMouseEnter={(e) => this.handleMouseEnter(e, 1)}
+            // onMouseLeave={(e) => this.handleMouseLeave(e, 1)}
+          >
+            {/* <div className='colorFormInner' /> */}
+          </div>
+          <div
+            className='colorFormOutline'
+            id='colorOuter2'
+            // onClick={(e) => this.handleClick(e, 2)}
+            // onMouseEnter={(e) => this.handleMouseEnter(e, 2)}
+            // onMouseLeave={(e) => this.handleMouseLeave(e, 2)}
+          >
+            {/* <div className='colorFormInner' /> */}
+          </div>
+          <div
+            className='colorFormOutline'
+            id='colorOuter3'
+            // onClick={(e) => this.handleClick(e, 3)}
+            // onMouseEnter={(e) => this.handleMouseEnter(e, 3)}
+            // onMouseLeave={(e) => this.handleMouseLeave(e, 3)}
+          >
+            {/* <div className='colorFormInner' /> */}
+          </div>
+          <div
+            className='colorFormOutline'
+            id='colorOuter4'
+            // onClick={(e) => this.handleClick(e, 4)}
+            // onMouseEnter={(e) => this.handleMouseEnter(e, 4)}
+            // onMouseLeave={(e) => this.handleMouseLeave(e, 4)}
+          >
+            {/* <div className='colorFormInner' /> */}
+          </div>
+          <div
+            className='colorFormOutline'
+            id='colorOuter5'
+            // onClick={(e) => this.handleClick(e, 5)}
+            // onMouseEnter={(e) => this.handleMouseEnter(e, 5)}
+            // onMouseLeave={(e) => this.handleMouseLeave(e, 5)}
+          >
+            {/* <div className='colorFormInner' /> */}
+          </div>
+          <div
+            className='colorFormOutline'
+            id='colorOuter6'
+            // onClick={(e) => this.handleClick(e, 6)}
+            // onMouseEnter={(e) => this.handleMouseEnter(e, 6)}
+            // onMouseLeave={(e) => this.handleMouseLeave(e, 6)}
+          >
+            {/* <div className='colorFormInner' /> */}
+          </div>
         </div>
       </div>
     );
--- src/Home.js
+++ src/Home.js
@@ -2,20 +2,46 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import './App.css';
 import Navbar from './Navbar.js';
-import ElementAvoid from './ElementAvoid.js';
+// import ElementAvoid from './ElementAvoid.js';
 import ColorController from './ColorController';
 
-class Home extends React.Component {
-  //   constructor(props) {
-  //     super(props);
-  //   }
+import { colors, colorsRGBA } from './colors.js';
 
-  componentDidMount() {}
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { selected: 4 };
+    this.getData = this.getData.bind(this);
+  }
+
+  getData(num) {
+    // do not forget to bind getData in constructor
+    console.log(`updated in home component: ${num}`);
+    this.setState({
+      selected: num,
+    });
+    document.body.style.backgroundColor = colors[num];
+    const list = document.getElementsByTagName('A');
+    if (num === 0) {
+      document.body.style.color = '#4d4d4d';
+
+      for (let i = 0; i < list.length; i++) {
+        list[i].style.color = '#4d4d4d';
+      }
+    } else {
+      document.body.style.color = 'white';
+      for (let i = 0; i < list.length; i++) {
+        list[i].style.color = 'white';
+      }
+    }
+  }
+
+  componentDidUpdate() {}
 
   render() {
     return (
-      <div>
-        <Navbar />
+      <div id='mainWrapper'>
+        <Navbar color={this.state.selected} />
         <p>HOME</p>
 
         <div id='avoidContainer'>
@@ -32,7 +58,7 @@ class Home extends React.Component {
             left='60vw'
           /> */}
         </div>
-        <ColorController />
+        <ColorController sendData={this.getData} />
       </div>
     );
   }
