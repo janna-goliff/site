cf23bedb5d2ce80f42f366a06ff574285de21346
janna-goliff <gojanna@sas.upenn.edu>
2021-04-13 16:32:45 -0400

    map proj desc

--- src/pages/MapProject1.js
+++ src/pages/MapProject1.js
@@ -24,7 +24,9 @@ class MapProject1 extends React.Component {
     return (
       <div>
         <div className='bumper'></div>
+        <h2>A GLIMPSE AT PHILDELPHIA HEALTH AND EDUCATION</h2>
         <div
+          id='subprojectContainer'
           style={{
             display: 'flex',
             flexDirection: 'column',
@@ -76,6 +78,86 @@ class MapProject1 extends React.Component {
               style={{ width: '14.6vw' }}
             />
           </div>
+          <p className='subProjectDescription'>
+            Using data from OpenDataPhilly.org, I wanted to get a look of the
+            data surrounding available health resources provided by the
+            Philadelphia Department of Public Health, seeing where ther were
+            located, and discovering what this means in terms of the city's
+            perception of need in different areas.
+          </p>
+          <p className='subProjectDescription emphasisLink'>
+            View sources here:
+            <ul>
+              <li>
+                <a href='https://www.opendataphilly.org/dataset/schools'>
+                  School data:{' '}
+                </a>
+                includes public schools, charter schools, many private schools,
+                school annexes, and athletic fields and facilities
+              </li>
+              <li>
+                <a href='https://www.opendataphilly.org/dataset/philadelphia-universities-and-colleges'>
+                  University and college data:{' '}
+                </a>
+                includes a list of all colleges and universities in Phildelphia
+              </li>
+              <li>
+                <a href='https://www.opendataphilly.org/dataset/philadelphia-hospitals'>
+                  Hospitals:{' '}
+                </a>
+                includes a list of all hospitals in Phildelphia
+              </li>
+              <li>
+                <a href='https://www.opendataphilly.org/dataset/health-centers'>
+                  Healthcare centers data:{' '}
+                </a>
+                includes a list of all primary care health centers in
+                Phildelphia
+              </li>
+              <li>
+                <a href='https://www.opendataphilly.org/dataset/sharps-drop-boxes'>
+                  "Sharps" (needle) drop boxes data
+                </a>
+              </li>
+              <li>
+                <a href='https://www.opendataphilly.org/dataset/hiv-testing-sites'>
+                  HIV Testing Sites
+                </a>
+              </li>
+              <li>
+                <a href='https://www.opendataphilly.org/dataset/prep-providers'>
+                  PrEP Providers
+                </a>
+              </li>
+              <li>
+                <a href='https://www.opendataphilly.org/dataset/condom-distribution-sites'>
+                  Condom distribution sites
+                </a>
+              </li>
+            </ul>
+          </p>
+        </div>
+        <div className='center'>
+          <div className='threeRow'>
+            <div className='individualCity flex-col'>
+              <p>University City</p>
+              <img src='https://i.imgur.com/88NXaqr.png' />
+            </div>
+            <div className='individualCity flex-col'>
+              <p>Washington Square</p>
+              <img src='https://i.imgur.com/j3Cdqt0.png' />
+            </div>
+            <div className='individualCity flex-col'>
+              <p>Upper Kensington</p>
+              <img src='https://i.imgur.com/W7Cyzna.png' />
+            </div>
+          </div>
+          <p className='subProjectDescription'>
+            {' '}
+            A look at three different areas. The University City area has a
+            significant amount of health-related resources, especially in
+            comparison to areas in Kensington.
+          </p>
         </div>
       </div>
     );
