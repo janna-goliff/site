504429d592195c3f80df217638c7b4b083d9a11b
Janna Goliff <gojanna@sas.upenn.edu>
2022-01-02 22:18:12 -0500

    navbar link rerevert

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import './navbar.scss';
 function Navbar() {
     return (
         <div className="navbar_wrapper">
-            <Link to="/" className="navbar_name">Janna Goliff</Link>
+            <Link to="/site/" className="navbar_name">Janna Goliff</Link>
             <div className="navbar_links">
                 <div id='dropdownMain2' className='navoption main_section_wrapper'>
                     <div id='devNav'>
@@ -13,10 +13,10 @@ function Navbar() {
                         <div className="underline" />
                     </div>
                     <div id='dropdownContent2'>
-                        <Link to='/engineering/futurepiproject'>2050: an Earth Odyssey</Link>
-                        <Link to='/webdev/beepboop'>Beepboop internship</Link>
-                        <Link to='/webdev/dataportraits'>Data portraits</Link>
-                        <Link to='/webdev/treehouse'>Web app development (CIS557)</Link>
+                        <Link to='/site/engineering/futurepiproject'>2050: an Earth Odyssey</Link>
+                        <Link to='/site/webdev/beepboop'>Beepboop internship</Link>
+                        <Link to='/site/webdev/dataportraits'>Data portraits</Link>
+                        <Link to='/site/webdev/treehouse'>Web app development (CIS557)</Link>
                     </div>
                 </div>
                 <div id='dropdownMain' className='navoption main_section_wrapper'>
@@ -25,19 +25,19 @@ function Navbar() {
                         <div className="underline" />
                     </div>
                     <div id='dropdownContent'>
-                        <Link to='/design/biennialredesign'>Biennial identity redesign</Link>
-                        <Link to='/design/webdesignexercise1'>Web design: book sharing site</Link>
-                        <Link to='/design/appdesign1'>App design: event tracker</Link>
-                        <Link to='/design/dataviz1'>Data viz: video game history</Link>
-                        <Link to='/design/logodesign1'>Grace's Organics logo</Link>
+                        <Link to='/site/design/biennialredesign'>Biennial identity redesign</Link>
+                        <Link to='/site/design/webdesignexercise1'>Web design: book sharing site</Link>
+                        <Link to='/site/design/appdesign1'>App design: event tracker</Link>
+                        <Link to='/site/design/dataviz1'>Data viz: video game history</Link>
+                        <Link to='/site/design/logodesign1'>Grace's Organics logo</Link>
                     </div>
                 </div>
                 <div className="main_section_wrapper">
-                    <Link className="main_section" to="/funstuff">Fun stuff</Link>
+                    <Link className="main_section" to="/site/funstuff">Fun stuff</Link>
                     <div className="underline" />
                 </div>
                 <div className="main_section_wrapper">
-                    <Link className="main_section" to="/about">About</Link>
+                    <Link className="main_section" to="/site/about">About</Link>
                     <div className="underline" />
                 </div>
             </div>
