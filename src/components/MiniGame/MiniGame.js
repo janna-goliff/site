dd0c3828b8511b6cfcd98ca5693c189423d3ee11
Janna Goliff <gojanna@sas.upenn.edu>
2022-01-11 19:10:11 -0500

    minigame debugging

--- src/components/MiniGame/MiniGame.js
+++ src/components/MiniGame/MiniGame.js
@@ -12,40 +12,55 @@ function MiniGame(props) {
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     ];
 
-    const [arrState, setArrState] = useState(arr);
-    const [rowPos, setRowPos] = useState(3);
-    const [colPos, setColPos] = useState(0);
+    const [position, setPosition] = useState({
+        row: 3,
+        col: 0,
+    });
+    // const [arrState, setArrState] = useState(arr);
+    // const [rowPos, setRowPos] = useState(3);
+    // const [colPos, setColPos] = useState(0);
 
     function move(direction) {
-        console.log("row: ", rowPos);
-        console.log("col: ", colPos);
-        const rowCopy = rowPos;
-        const colCopy = colPos;
+        console.log("row: ", position.row);
+        console.log("col: ", position.col);
+        const rowCopy = position.row;
+        const colCopy = position.col;
         // move up if in bounds
-        if (direction === "KeyW" && rowPos > 0) {
+        if (direction === "KeyW" && rowCopy > 0) {
             console.log("UP");
-            setRowPos(rowCopy - 1);
+            setPosition({
+                row: rowCopy - 1,
+                col: colCopy,
+            });
         }
         // move down if in bounds
-        else if (direction === "KeyS" && rowPos < arr.length) {
+        else if (direction === "KeyS" && rowCopy < arr.length) {
             console.log("DOWN");
-            setRowPos(rowCopy + 1);
+            setPosition({
+                row: rowCopy + 1,
+                col: colCopy,
+            });
         }
         // move left if in bounds
-        else if (direction === "KeyA" && colPos > 0) {
+        else if (direction === "KeyA" && colCopy > 0) {
             console.log("LEFT");
-            setColPos(colCopy - 1);
+            setPosition({
+                row: rowCopy,
+                col: colCopy - 1,
+            });
         }
         // move right if in bounds
-        else if (direction === "KeyD" && colPos < arr[0].length) {
+        else if (direction === "KeyD" && colCopy < arr[0].length) {
             console.log("RIGHT to: ", colCopy + 1);
-            setColPos(colCopy + 1);
-            console.log("now at: ", + rowPos + ", " + colPos);
+            setPosition({
+                row: rowCopy,
+                col: colCopy + 1,
+            });
         }
     }
 
     
-    document.addEventListener('keyup', (e) => {
+    document.addEventListener('keydown', (e) => {
         console.log(e.code);
         move(e.code);
     });
@@ -58,24 +73,21 @@ function MiniGame(props) {
     }, []);
 
     useEffect(() => {
-        console.log("Row changed: ", rowPos)
-    }, [rowPos]);
-    useEffect(() => {
-        console.log("Col changed: ", colPos)
-    }, [colPos]);
+        console.log("Row : ", position.row, "column: ", position.col);
+    }, [position]);
 
     return (
         <div className="game_container">
            {
-               arrState.map((row, rowNum) => {
+               arr.map((row, rowNum) => {
                    return (
-                       <div className="row">
+                       <div key={rowNum + "coord"} className="row">
                            {row.map((coordinate, colNum) => {
                                return (
-                                   (rowPos === rowNum) && (colPos === colNum) ?
-                                    <div className={"coordinate"}></div>
+                                   (position.row === rowNum) && (position.col === colNum) ?
+                                    <div key={colNum + "coord"} className={"coordinate"}></div>
                                     :
-                                    <div className={"coordinate current"}></div>
+                                    <div key={colNum + "curr_coord"} className={"coordinate current"}></div>
                                 )
                             }
                            )}
