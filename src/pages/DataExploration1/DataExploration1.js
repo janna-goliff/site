670c8a26f598929dd1f1c0be0ad738fec1effe76
Janna Goliff <gojanna@sas.upenn.edu>
2024-02-21 01:04:03 -0500

    comment cleanup

--- src/pages/DataExploration1/DataExploration1.js
+++ src/pages/DataExploration1/DataExploration1.js
@@ -1,30 +1,125 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import * as d3 from 'd3';
 import { Scrollama, Step } from 'react-scrollama';
 import Navbar from "../../components/Navbar/Navbar";
 import ColorChanger from "../../components/ColorChanger/ColorChanger";
 import "./dataexploration1.scss";
 
+let dataLocation = 'https://raw.githubusercontent.com/janna-goliff/site/main/src/assets/intercountry_adoption_into_US_over_2001_2020.csv';
 
+function barChart() {
+    let svg = d3.select('#barChart');
+    let margin = 350;
+    let width = svg.attr('width') - margin;
+    let height = svg.attr('height') - margin;
+
+    let xScale = d3.scaleBand().range([0, width]).padding(0.4);
+    let yScale = d3.scaleLinear().range([height, 0]);
+    let g = svg
+        .append('g')
+        .attr('transform', 'translate(' + 175 + ',' + 100 + ')');
+
+    d3.csv(dataLocation).then(function (data) {
+        // format data into numbers
+        data.forEach(function (d) {
+            d.Number_of_Completed_Adoptions_to_US = parseInt(d.Number_of_Completed_Adoptions_to_US);
+        });
+
+        // group by year
+        let dataByYear = d3.group(data, (d) => d.Year);
+        
+        // sorts data by year, programatically choses a year, then sorts that year's data
+        // by highest # of adoptions, taking top 50 results 
+        dataByYear = dataByYear
+            .get("2001")
+            .sort((a, b) => d3.descending(a.Number_of_Completed_Adoptions_to_US, b.Number_of_Completed_Adoptions_to_US))
+            .slice(0, 50);
+
+        // set x domain to use country names
+        xScale.domain(
+            dataByYear.map(function (d) {
+                return d.Country;
+            })
+        );
+
+        // set y domain to use range of 0 to max # of adoptions
+        yScale.domain([0, dataByYear[0].Number_of_Completed_Adoptions_to_US]);
+
+        // x axis bottom scale
+        g.append('g')
+        .attr('class', 'x_axis')
+        .attr('transform', 'translate(0,' + height + ')')
+        .call(d3.axisBottom(xScale))
+
+
+        // x axis bottom scale labels, rotate and adjusted
+        g.selectAll('.tick text')
+        .attr('fill', 'black')
+        .attr('text-anchor', 'end')
+        .attr('dx', '-1em')
+        .attr('dy', '-.5em')
+        .attr('transform', 'rotate(-90)')
+        .attr('margin-right', "2em");
+
+        // y axis group element
+        g.append('g')
+        .call(
+        d3
+            .axisLeft(yScale)
+            .tickFormat(function (d) {
+            return d;
+            })
+            .ticks(10)
+            .tickSizeInner(-width) // sets tick lines to expand into grid lines
+        )
+        .attr('class', 'y_axis')
+        .append('text')
+        .attr('class', 'yAxisTextLabel')
+        .attr('y', 6)
+        .attr('dx', '-' + (height / 2)+ 'px')
+        .attr('dy', '-3em')
+        .text('Country');
+
+    g.selectAll('.domain').attr('stroke', 'black');
+    g.selectAll('.tick line').attr('stroke', 'black');
+    
+
+    g.selectAll('.bar')
+        .data(dataByYear)
+        .enter()
+        .append('rect')
+        .attr('class', 'bar')
+        .attr('x', function (d) {
+           // console.log(d);
+            return xScale(d.Country);
+        })
+        .attr('y', function (d) {
+            return yScale(d.Number_of_Completed_Adoptions_to_US);
+        })
+        .attr('width', xScale.bandwidth())
+        .transition()
+        .duration(1500)
+        .attr('height', function (d) {
+            return height - yScale(d.Number_of_Completed_Adoptions_to_US);
+        })
+        .attr('fill', function (d, i) {
+         return 'black';
+        });
+    });;
+}
 
 function DataExploration1() {
     // track what step of react-scrollama
     const [currentStepIndex, setCurrentStepIndex] = useState(null);
 
+    useEffect(()=>{
+        barChart();
+    }, []);
+
     // scrollama event handlers
     function onStepEnter({ data }) {
         setCurrentStepIndex(data);
     }
-
-    let data = 'https://raw.githubusercontent.com/janna-goliff/site/main/src/assets/intercountry_adoption_into_US_over_2001_2020.csv';
-    d3.csv(data, function(d) {
-        return {
-          num: d.Number_of_Completed_Adoptions_to_US,
-          year: d.Year,
-        };
-      }, function(error, rows) {
-        console.log(rows);
-      });
 
     return (
         <ColorChanger>
@@ -44,6 +139,9 @@ function DataExploration1() {
                         <br />
                         Explore the Github <a href="https://github.com/janna-goliff/dataartsite">here</a>.
                     </div>
+                </div>
+                <div className="container">
+                    <svg width='1300' height='800' id='barChart'></svg>
                 </div>
                 <div className="container">
                     <div style={{ display: 'flex', flexDirection: 'column', margin: '50vh 0', border: '2px dashed skyblue' }}>
