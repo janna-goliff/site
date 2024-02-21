b185effe3c94268d71f2a9c5ce3e8d2d06d9dbdc
Janna Goliff <gojanna@sas.upenn.edu>
2024-02-21 14:29:16 -0500

    bug fixes to bar chart

--- src/pages/DataExploration1/DataExploration1.js
+++ src/pages/DataExploration1/DataExploration1.js
@@ -5,33 +5,35 @@ import Navbar from "../../components/Navbar/Navbar";
 import ColorChanger from "../../components/ColorChanger/ColorChanger";
 import "./dataexploration1.scss";
 
-//let dataLocation = 'https://raw.githubusercontent.com/janna-goliff/site/main/src/assets/intercountry_adoption_into_US_over_2001_2020.csv';
+let dataLocation = 'https://raw.githubusercontent.com/janna-goliff/site/main/src/assets/intercountry_adoption_into_US_over_2001_2020.csv';
 
-let dataLocation = 'https://raw.githubusercontent.com/janna-goliff/site/main/src/assets/placeholder.csv';
+//let dataLocation = 'https://raw.githubusercontent.com/janna-goliff/site/main/src/assets/placeholder.csv';
+
+const translateX = 100;
+const translateY = 100;
+const numberOfCountries = 25;
 
 // takes a year indexed InternMap and string representing a year
-// returns data for the given year, sorted by highest to lowest adoptions top 50 countries
-function processAdoptionsByYearData(data, yearString) {
-    console.log("data in process: ", data);
-    console.log("yearString: ", yearString);
+// returns data for the given year, sorted by highest to lowest adoptions top X countries
+function processAdoptionsByYearData(data, year) {
     // grab by year
     let dataReturn = data
-        .get(yearString);
+        .get(year);
         console.log("dataReturn: ", dataReturn);
     dataReturn = dataReturn
         .sort((a, b) => d3.descending(
             parseInt(a.Number_of_Completed_Adoptions_to_US), 
             parseInt(b.Number_of_Completed_Adoptions_to_US)))
-        .slice(0, 50);
+        .slice(0, numberOfCountries);
 
         return dataReturn;
 }
 
 //add moving percentage horizontal bar chart?
-function barChart(adoptionData) {
+function createInitialBarChart(adoptionData) {
     // grab initial svg sizes
     let svg = d3.select('#barChartAdoptionsByYear');
-    let margin = 350;
+    let margin = 150;
     let width = svg.attr('width') - margin;
     let height = svg.attr('height') - margin;
 
@@ -40,100 +42,95 @@ function barChart(adoptionData) {
     let g = svg
         .append('g')
         .attr('id', 'barInner')
-        .attr('transform', 'translate(' + 175 + ',' + 100 + ')');
+        .attr('transform', 'translate(' + translateX + ',' + translateY + ')');
 
-    // grab and format data
-        
+    // grab by year to use for initial axis labels
+    let dataByYear = processAdoptionsByYearData(adoptionData, 2001);
 
-        // grab by year
-        let dataByYear = processAdoptionsByYearData(adoptionData, "2001");
-        console.log("dataByYear", dataByYear);
+    // set x domain to use country names
+    xScale.domain(
+        dataByYear.map(function (d) {
+            return d.Country;
+        })
+    );
 
-        // format Year column data into int
-        dataByYear.forEach(function (d) {
-            d.Number_of_Completed_Adoptions_to_US = parseInt(d.Number_of_Completed_Adoptions_to_US);
-        });
+    // set y domain to use range of 0 to max # of adoptions
+    yScale.domain([0, dataByYear[0].Number_of_Completed_Adoptions_to_US]);
 
-        // set x domain to use country names
-        xScale.domain(
-            dataByYear.map(function (d) {
-                return d.Country;
-            })
-        );
+    // x axis bottom scale
+    g.append('g')
+    .attr('class', 'x_axis')
+    .attr('transform', 'translate(0,' + height + ')')
+    .call(d3.axisBottom(xScale))
 
-        // set y domain to use range of 0 to max # of adoptions
-        yScale.domain([0, dataByYear[0].Number_of_Completed_Adoptions_to_US]);
+    // x axis bottom scale labels, rotate and adjusted
+    g.selectAll('.tick text')
+    .attr('fill', 'black')
+    .attr('text-anchor', 'end')
+    .attr('dx', '-1em')
+    .attr('dy', '-.5em')
+    .attr('transform', 'rotate(-90)')
+    .attr('margin-right', "2em");
 
-        // x axis bottom scale
-        g.append('g')
-        .attr('class', 'x_axis')
-        .attr('transform', 'translate(0,' + height + ')')
-        .call(d3.axisBottom(xScale))
-
-        // x axis bottom scale labels, rotate and adjusted
-        g.selectAll('.tick text')
-        .attr('fill', 'black')
-        .attr('text-anchor', 'end')
-        .attr('dx', '-1em')
-        .attr('dy', '-.5em')
-        .attr('transform', 'rotate(-90)')
-        .attr('margin-right', "2em");
-
-        // y axis group element
-        g.append('g')
-        .call(
-        d3
-            .axisLeft(yScale)
-            .tickFormat(function (d) {
-            return d;
-            })
-            .ticks(10)
-            .tickSizeInner(-width) // sets tick lines to expand into grid lines
-        )
-        .attr('class', 'y_axis')
-        .append('text') // create text lbale for y axis
-        .attr('class', 'yAxisTextLabel')
-        .attr('y', 6)
-        .attr('dx', '-' + (height / 2)+ 'px')
-        .attr('dy', '-3em')
-        .text('Country');
-
-    // g.selectAll('.bar')
-    //     .data(dataByYear)
-    //     .enter()
-    //     .append('rect')
-    //     .attr('class', 'bar')
-        // .attr('x', function (d) {
-        //     return xScale(d.Country);
-        // })
-        // .attr('y', function (d) {
-        //     return yScale(d.Number_of_Completed_Adoptions_to_US);
-        // })
-        // .attr('width', xScale.bandwidth())
-        // .transition()
-        // .duration(1500)
-        // .attr('height', function (d) {
-        //     return height - yScale(d.Number_of_Completed_Adoptions_to_US);
-        // })
-        // .attr('fill', function (d, i) {
-        //  return 'black';
-        // });
+    // y axis group element
+    g.append('g')
+    .call(
+    d3
+        .axisLeft(yScale)
+        .tickFormat(function (d) {
+        return d;
+        })
+        .ticks(10)
+        .tickSizeInner(-width) // sets tick lines to expand into grid lines
+    )
+    .attr('class', 'y_axis')
+    .append('text') // create text lbale for y axis
+    .attr('class', 'yAxisTextLabel')
+    .attr('y', 6)
+    .attr('dx', '-' + (height / 2)+ 'px')
+    .attr('dy', '-3em')
+    .text('Country');
 }
 
 // A function that create / update the plot for a given variable:
 function updateBarChart(adoptionData, year) {
     let svg = d3.select("#barChartAdoptionsByYear");
-    let margin = 350;
+    let margin = 150;
     let width = svg.attr('width') - margin;
     let height = svg.attr('height') - margin;
-    console.log(width, height)
 
     let xScale = d3.scaleBand().range([0, width]).padding(0.4);
     let yScale = d3.scaleLinear().range([height, 0]);
 
-    console.log("adoptionData in update: ", adoptionData, year)
-    console.log("year in update:", year)
-    let data = processAdoptionsByYearData(adoptionData, year.toString());
+    let data = processAdoptionsByYearData(adoptionData, year);
+
+     // set x domain to use country names
+     xScale.domain(
+        data.map(function (d) {
+            return d.Country;
+        })
+    );
+    
+    d3.select(".x_axis").transition().duration(1000).call(d3.axisBottom(xScale))
+    .selectAll('.tick text')
+    .attr('fill', 'black')
+    .attr('text-anchor', 'end')
+    .attr('dx', '-1em')
+    .attr('dy', '-.5em')
+    .attr('transform', 'rotate(-90)')
+    .attr('margin-right', "2em"); // x axis bottom scale labels, rotate and adjusted
+
+    // set y domain to use range of 0 to max # of adoptions
+    yScale.domain([0, data[0].Number_of_Completed_Adoptions_to_US]);
+    d3.select(".y_axis").transition().duration(1000).call(
+        d3
+        .axisLeft(yScale)
+        .tickFormat(function (d) {
+        return d;
+        })
+        .ticks(10)
+        .tickSizeInner(-width) // sets tick lines to expand into grid lines
+    );
 
     // Create the u variable
     var u = svg.selectAll("rect")
@@ -146,36 +143,13 @@ function updateBarChart(adoptionData, year) {
     .transition() // and apply changes to all of them
     .duration(1000)
         .attr("x", function(d) { return xScale(d.Country); })
-        .attr("y", function(d) { return yScale(d.Number_of_Completed_Adoptions_to_US); })
+        .attr("y", function(d) { 
+            return yScale(d.Number_of_Completed_Adoptions_to_US); })
         .attr("width", xScale.bandwidth())
         .attr("height", function(d) { return height - yScale(d.Number_of_Completed_Adoptions_to_US); })
+        .attr('transform', 'translate(' + translateX + ',' + translateY + ')')
         .attr("fill", "#69b3a2")
-
-
-    //     let g = d3.select("#barInner").selectAll('.bar');
-    //     g
-    //     .data(data)
-    //     .enter()
-    //     .append('rect')
-    //     .attr('class', 'bar')
-    //   .enter()
-    //   .append("rect")
-    //   .merge(g)
-    //   .transition()
-    //   .duration(1000)
-    //   .attr('x', function (d) {
-    //     return xScale(d.Country);
-    //     })
-    //     .attr('y', function (d) {
-    //         return yScale(d.Number_of_Completed_Adoptions_to_US);
-    //     })
-    //     .attr('width', xScale.bandwidth())
-    //     .transition()
-    //     .duration(1500)
-    //     .attr('height', function (d) {
-    //         return height - yScale(d.Number_of_Completed_Adoptions_to_US);
-    //     })
-    //     .attr("fill", "#69b3a2")
+        .attr("class", ".bar")
   }
 
 function DataExploration1() {
@@ -185,17 +159,24 @@ function DataExploration1() {
 
     useEffect(() => {
         d3.csv(dataLocation).then(function (data) {
+            // format data into numbers
+            data.forEach(function (d) {
+                d.Year = parseInt(d.Year);
+                d.Number_of_Completed_Adoptions_to_US = parseInt(d.Number_of_Completed_Adoptions_to_US);
+                if (isNaN(d.Number_of_Completed_Adoptions_to_US)) {
+                    d.Number_of_Completed_Adoptions_to_US = 0;
+                }
+            });
             const dataGroupedByYear = d3.group(data, (d) => d.Year);
+            
             setAdoptionData(dataGroupedByYear);
-            barChart(dataGroupedByYear);
-            updateBarChart( dataGroupedByYear, "2001");
-            console.log("data: ", dataGroupedByYear);
+            createInitialBarChart(dataGroupedByYear);
+            updateBarChart( dataGroupedByYear, 2001);
         })
     }, []);
 
     // scrollama event handlers
     function onStepEnter({ data }) {
-        console.log("adoptionData on step enter: ", adoptionData);
         setCurrentStepIndex(data); 
         updateBarChart( adoptionData, data < 3 ? (2001 + data) : (2002 + data));
     }
@@ -222,7 +203,7 @@ function DataExploration1() {
                 <div className="container">
                     <div style={{ display: 'flex', flexDirection: 'column', margin: '50vh 0', border: '2px dashed skyblue' }}>
                         <div style={{ position: 'sticky', top: 0, border: '1px solid orchid' }}>
-                            I'm sticky. The current triggered step index is: {currentStepIndex}
+                            {/* I'm sticky. The current triggered step index is: {currentStepIndex} */}
                             <svg width='1300' height='800' id='barChartAdoptionsByYear'></svg>
                         </div>
                         <Scrollama offset={0.5} onStepEnter={onStepEnter} debug>
