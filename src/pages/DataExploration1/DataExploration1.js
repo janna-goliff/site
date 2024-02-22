bb6fff35ef2c3cb58f3a907348e0a8629ddb74c0
Janna Goliff <gojanna@sas.upenn.edu>
2024-02-22 08:24:14 -0500

    manual data cleanup

--- src/pages/DataExploration1/DataExploration1.js
+++ src/pages/DataExploration1/DataExploration1.js
@@ -5,47 +5,44 @@ import Navbar from "../../components/Navbar/Navbar";
 import ColorChanger from "../../components/ColorChanger/ColorChanger";
 import "./dataexploration1.scss";
 
+/**
+ * Helpful tutorials:
+ * https://d3-graph-gallery.com/graph/barplot_button_data_hard.html
+ */
+
 let dataLocation = 'https://raw.githubusercontent.com/janna-goliff/site/main/src/assets/intercountry_adoption_into_US_over_2001_2020.csv';
 
 //let dataLocation = 'https://raw.githubusercontent.com/janna-goliff/site/main/src/assets/placeholder.csv';
 
 const translateX = 100;
 const translateY = 100;
+// controls how many countries are rendered in bar chart
 const numberOfCountries = 25;
 
 // takes a year indexed InternMap and string representing a year
 // returns data for the given year, sorted by highest to lowest adoptions top X countries
 function processAdoptionsByYearData(data, year) {
     // grab by year
-    let dataReturn = data
-        .get(year);
-        console.log("dataReturn: ", dataReturn);
-    dataReturn = dataReturn
+    return data
+        .get(year)
         .sort((a, b) => d3.descending(
             parseInt(a.Number_of_Completed_Adoptions_to_US), 
             parseInt(b.Number_of_Completed_Adoptions_to_US)))
         .slice(0, numberOfCountries);
-
-        return dataReturn;
 }
 
-//add moving percentage horizontal bar chart?
-function createInitialBarChart(adoptionData) {
-    // grab initial svg sizes
-    let svg = d3.select('#barChartAdoptionsByYear');
-    let margin = 150;
+// runs initial steps to create bar chart - calculates width, height, xScale, yScale
+function initializeBarChart(barChartId, year, data) {
+    let svg = d3.select(barChartId);
+
+    let margin = 200;
     let width = svg.attr('width') - margin;
     let height = svg.attr('height') - margin;
 
     let xScale = d3.scaleBand().range([0, width]).padding(0.4);
     let yScale = d3.scaleLinear().range([height, 0]);
-    let g = svg
-        .append('g')
-        .attr('id', 'barInner')
-        .attr('transform', 'translate(' + translateX + ',' + translateY + ')');
 
-    // grab by year to use for initial axis labels
-    let dataByYear = processAdoptionsByYearData(adoptionData, 2001);
+    let dataByYear = processAdoptionsByYearData(data, year);
 
     // set x domain to use country names
     xScale.domain(
@@ -57,6 +54,21 @@ function createInitialBarChart(adoptionData) {
     // set y domain to use range of 0 to max # of adoptions
     yScale.domain([0, dataByYear[0].Number_of_Completed_Adoptions_to_US]);
 
+    return {height, width, xScale, yScale, dataByYear};
+}
+
+// add moving percentage horizontal bar chart?
+function createInitialBarChart(adoptionData) {
+    // grab initial svg sizes
+    let svg = d3.select('#barChartAdoptionsByYear');
+    
+    let g = svg
+        .append('g')
+        .attr('id', 'barInner')
+        .attr('transform', 'translate(' + translateX + ',' + translateY + ')');
+
+    const { height, width, xScale, yScale } = initializeBarChart('#barChartAdoptionsByYear', 2001, adoptionData);
+    
     // x axis bottom scale
     g.append('g')
     .attr('class', 'x_axis')
@@ -69,7 +81,7 @@ function createInitialBarChart(adoptionData) {
     .attr('text-anchor', 'end')
     .attr('dx', '-1em')
     .attr('dy', '-.5em')
-    .attr('transform', 'rotate(-90)')
+    .attr('transform', 'rotate(-60)')
     .attr('margin-right', "2em");
 
     // y axis group element
@@ -78,50 +90,42 @@ function createInitialBarChart(adoptionData) {
     d3
         .axisLeft(yScale)
         .tickFormat(function (d) {
-        return d;
+            return d;
         })
         .ticks(10)
         .tickSizeInner(-width) // sets tick lines to expand into grid lines
     )
     .attr('class', 'y_axis')
-    .append('text') // create text lbale for y axis
+    .selectAll('.tick text')
+    .attr('dx', '-1em')
+    .append('text') // create text label for y axis
     .attr('class', 'yAxisTextLabel')
     .attr('y', 6)
     .attr('dx', '-' + (height / 2)+ 'px')
     .attr('dy', '-3em')
     .text('Country');
+    
 }
 
 // A function that create / update the plot for a given variable:
 function updateBarChart(adoptionData, year) {
     let svg = d3.select("#barChartAdoptionsByYear");
-    let margin = 150;
-    let width = svg.attr('width') - margin;
-    let height = svg.attr('height') - margin;
+    // let tooltipDiv = svg.append("div")
+    //  .attr("class", "tooltip-bar")
+    //  .style("opacity", 0)
+    //  .style("position", "fixed");
 
-    let xScale = d3.scaleBand().range([0, width]).padding(0.4);
-    let yScale = d3.scaleLinear().range([height, 0]);
+    const { height, width, xScale, yScale, dataByYear } = initializeBarChart('#barChartAdoptionsByYear', year, adoptionData);
 
-    let data = processAdoptionsByYearData(adoptionData, year);
-
-     // set x domain to use country names
-     xScale.domain(
-        data.map(function (d) {
-            return d.Country;
-        })
-    );
-    
     d3.select(".x_axis").transition().duration(1000).call(d3.axisBottom(xScale))
     .selectAll('.tick text')
     .attr('fill', 'black')
     .attr('text-anchor', 'end')
     .attr('dx', '-1em')
     .attr('dy', '-.5em')
-    .attr('transform', 'rotate(-90)')
+    .attr('transform', 'rotate(-60)')
     .attr('margin-right', "2em"); // x axis bottom scale labels, rotate and adjusted
-
-    // set y domain to use range of 0 to max # of adoptions
-    yScale.domain([0, data[0].Number_of_Completed_Adoptions_to_US]);
+    
     d3.select(".y_axis").transition().duration(1000).call(
         d3
         .axisLeft(yScale)
@@ -130,17 +134,34 @@ function updateBarChart(adoptionData, year) {
         })
         .ticks(10)
         .tickSizeInner(-width) // sets tick lines to expand into grid lines
-    );
+    )
+    .selectAll('.tick text')
+    .attr('dx', '-1em');
 
     // Create the u variable
     var u = svg.selectAll("rect")
-    .data(data)
+    .data(dataByYear)
 
-    u
+    const rect = u
     .enter()
     .append("rect") // Add a new rect for each new elements
-    .merge(u) // get the already existing elements as well
-    .transition() // and apply changes to all of them
+    .merge(u);  // get the already existing elements as well
+
+    // rect
+    // .on('mouseover', function(d, i) {
+    //     console.log(d)
+    //     tooltipDiv
+    //     .transition().duration(200)
+    //     .style('opacity', 1);
+    //     tooltipDiv.html(i.Number_of_Completed_Adoptions_to_US)
+    //     .attr('x', d.clientX)
+    //     .attr('y', d.clientY);
+    //   })
+    // .on('mouseout', function(d, i) {
+    //     tooltipDiv.style('opacity', 0)
+    //     });
+
+    rect.transition() // and apply changes to all of them
     .duration(1000)
         .attr("x", function(d) { return xScale(d.Country); })
         .attr("y", function(d) { 
@@ -148,8 +169,9 @@ function updateBarChart(adoptionData, year) {
         .attr("width", xScale.bandwidth())
         .attr("height", function(d) { return height - yScale(d.Number_of_Completed_Adoptions_to_US); })
         .attr('transform', 'translate(' + translateX + ',' + translateY + ')')
-        .attr("fill", "#69b3a2")
-        .attr("class", ".bar")
+        .attr("fill", function(d) {
+            return d.Country === "China" ? "#d60f04" : "var(--mainContentColor"})
+        .attr("class", ".bar");
   }
 
 function DataExploration1() {
@@ -204,14 +226,15 @@ function DataExploration1() {
                     <div style={{ display: 'flex', flexDirection: 'column', margin: '50vh 0', border: '2px dashed skyblue' }}>
                         <div style={{ position: 'sticky', top: 0, border: '1px solid orchid' }}>
                             {/* I'm sticky. The current triggered step index is: {currentStepIndex} */}
-                            <svg width='1300' height='800' id='barChartAdoptionsByYear'></svg>
+                            Year is: { currentStepIndex < 3 ? (2001 + currentStepIndex) : (2002 + currentStepIndex) }
+                            <svg width='1300' height='600' id='barChartAdoptionsByYear'></svg>
                         </div>
-                        <Scrollama offset={0.5} onStepEnter={onStepEnter} debug>
-                            {[1, 2, 3, 4].map((_, stepIndex) => (
+                        <Scrollama offset={0.3} onStepEnter={onStepEnter} debug>
+                            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19].map((_, stepIndex) => (
                             <Step data={stepIndex} key={stepIndex}>
                                 <div
                                 style={{
-                                    margin: '50vh 0',
+                                    margin: '30vh 0',
                                     border: '1px solid gray',
                                     opacity: currentStepIndex === stepIndex ? 1 : 0.2,
                                     padding: '10em'
