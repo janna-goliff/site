09ae449b1426debf92fff1487daf1e59e11ab2f3
Janna Goliff <gojanna@sas.upenn.edu>
2024-02-29 11:22:11 -0500

    style updates

--- src/pages/DataExploration1/ChinaAdoptionTimeline/ChinaAdoptionTimeline.js
+++ src/pages/DataExploration1/ChinaAdoptionTimeline/ChinaAdoptionTimeline.js
@@ -3,6 +3,8 @@ import * as d3 from 'd3';
 import { Scrollama, Step } from 'react-scrollama';
 import "./chinaadoptiontimeline.scss";
 
+import family_planning_70s from "../../../assets/dataexploration1/familly_planning_70s.jpeg"
+
 /**
  * Helpful tutorials:
  * https://d3-graph-gallery.com/graph/barplot_button_data_hard.html
@@ -11,6 +13,8 @@ import "./chinaadoptiontimeline.scss";
 // Data from https://travel.state.gov/content/travel/en/Intercountry-Adoption/adopt_ref/adoption-statistics-esri.html?wcmmode=disabled
 const chinaAdoptionData = {
     1988: 12,
+    1992: 12, //artifical
+    1993: 12, //artifical
     1995: 2000,
     1999: 4108,
     2000: 5058,
@@ -102,7 +106,7 @@ function updateDotChart(year) {
   }
 
 function calculateYear(stepIndex) {
-    const year = [1979, 1988, 1992, 1995, 1997, 2001, 2002, 2004, 2007, 2010, 2015, 2021][stepIndex]
+    const year = ["1970s", 1979, 1988, 1992, 1995, 1997, 2001, 2002, 2004, 2007, 2010, 2015, 2021][stepIndex]
     return year;
 }
 
@@ -111,9 +115,37 @@ function getYearInfo(year) {
     const yearCaptions = {
         "1970s": "China releases large amounts of family-planning materials to encourage a lower birth rate.",
         "1979": "China implements the one-child policy, mandating that couples only have one child or face a variety of different penalties.",
-        "1988": "Few records of children adopted from China to the US; at this point, the international adoption process is heavily regulated and difficult to navigate.\n\nChina soon makes the adoption process more accessible to foreigners, with the regulation that “only foreigners of Chinese heritage, non-Chinese foreigners with strong ties to the country, or long-term foreign residents of China were eligible to adopt”."
+        "1988": "Records show 12 children adopted from China to the US; at this point, the international adoption process is heavily regulated and difficult to navigate.\n\nChina soon makes the adoption process more accessible to foreigners, with the regulation that “only foreigners of Chinese heritage, non-Chinese foreigners with strong ties to the country, or long-term foreign residents of China were eligible to adopt”.",
+        "1992": "China enacts the Adoption Law of China, establishing a national policy for foreign adoption, and streamlining the adoption process for foreign couples.",
+        "1993": "The Hague Convention on the Protection of Children and Co-operation in Respect of Intercountry Adoption, an international agreement to safeguard intercountry adoptions, is established. The US signed a year later.",
+        "1995": "2,000+ children are adopted from China into the US, almost all infants or toddler girls.",
+        "1997": "In the US, The Small Business and Job Protection Act of 1996 creates an adoption tax credit with a maximum amount of $5,000.",
+        "2001": "The Economic Growth and Tax Relief Reconciliation Act of 2001 temporarily extended and expanded the adoption tax credit, doubling it from $5,000 to $10,000.",
+        "2015": "China's one-child limit was increased to allow two children, decreasing the pool of adoptable children.",
+        "2021": "China's one-child policy was relaxed even further to allow for three children."
     }
     return yearCaptions[yearKey] ? yearCaptions[yearKey] : "";
+}
+
+function getYearContent(year) {
+    const yearKey = year.toString();
+    if (yearKey === "1970s") {
+        return (
+        <div className="full_image_spread">
+            <img src={family_planning_70s} alt="Poster from 1970s promoting family planning in China" />
+            <div className="caption">
+                "Carry out family planning for the revolution" from Shanghai People's Fine Arts Publisher, 1974
+                <a className="caption_link" target="_blank" rel="noopener noreferrer" href="https://www.nlm.nih.gov/hmd/topics/chinese-family/family-politics_101557845-sm.html?imgid=10">Source</a>
+            </div>
+        </div>)
+    }
+}
+
+function renderNonSvgContent(year) {
+    const yearKey = year.toString();
+    const nonSvgYears = ["1970s"]
+    const isNonSvgYear = nonSvgYears.find((val) => { return val === yearKey})
+    return isNonSvgYear !== undefined;
 }
 
 function ChinaAdoptionTimeline() {
@@ -137,29 +169,38 @@ function ChinaAdoptionTimeline() {
 
     return (
         <div className="chinaAdoptionTimelineContainer">
+            <div className="scrolly">
+                <Scrollama offset={0.3} onStepEnter={onStepEnter}>
+                    {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((_, stepIndex) => (
+                    <Step data={stepIndex} key={stepIndex}>
+                        <div className="stepContainer">
+                            <div className="yearInfoContainer">
+                                <div className="yearTextContainer">
+                                    <div className="yearLine"></div>
+                                    <div className="yearTextLabel">{calculateYear(stepIndex)}</div>
+                                </div>
+                                <div className="yearInfo">{getYearInfo(calculateYear(stepIndex))}</div>
+                            </div>
+                        I'm a Scrollama Step of index {stepIndex}
+                        {
+                            renderNonSvgContent(calculateYear(stepIndex)) && 
+                            getYearContent(calculateYear(stepIndex)) 
+                        }
+                        </div>
+                    </Step>
+                    ))}
+                </Scrollama>
+            </div>
             <div className="year">
                 {/* I'm sticky. The current triggered step index is: {currentStepIndex} */}
-                <div className="yearInfoContainer">
+                {/* <div className="yearInfoContainer">
                     <div className="yearTextContainer">
                         <div className="yearLine"></div>
                         <div className="yearTextLabel">{calculateYear(currentStepIndex)}</div>
                     </div>
                     <div className="yearInfo">{getYearInfo(calculateYear(currentStepIndex))}</div>
-                </div>
+                </div> */}
                 <svg width='600' height='1000' id='dotChartChinaAdoptionsByYear'></svg>
-            </div>
-            <div className="scrolly">
-                <Scrollama offset={0.3} onStepEnter={onStepEnter}>
-                    {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((_, stepIndex) => (
-                    <Step data={stepIndex} key={stepIndex}>
-                        
-                        <div className="stepContainer">
-                        I'm a Scrollama Step of index {stepIndex}
-                        {stepIndex === 1 ? "China one child policy" : ""}
-                        </div>
-                    </Step>
-                    ))}
-                </Scrollama>
             </div>
         </div>
     );
